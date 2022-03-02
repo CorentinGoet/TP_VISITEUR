22382ab80a9de336a7c8ea61d20357e4506952d3
Guillian <guillianbillard@gmail.com>
2022-03-02 17:18:21 +0100

    Parser + part of visitor

--- visiteur.py
+++ visiteur.py
@@ -4,6 +4,23 @@
 import ast
 
 class Visiteur:
+    def init(self):
+        pass
+
+    def visitProgram(self, program):
+        print("Program{")
+        self.visitDeclarations(program.getDeclarations())
+        #self.visitStatements(program.getDeclarations())
+        print("}\n")
+
+    def visitDeclarations(self, declarations):
+        print("Declarations{")
+        for d in declarations.declaration:
+            self.visitDeclaration(d)
+        print("}\n")
+
+    def visitDeclaration(self, declaration):
+        print("Declaration{Type:" + str(declaration.type) + "|Identifier:" + str(declaration.identifier)+"\n");
 
 
 if __name__ == '__main__':
