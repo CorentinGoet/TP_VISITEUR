f00a28704ef14e31c3d680734ed68108c4c4239c
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-03-02 15:59:37 +0100

    added declaration classes

---
+++ declaration.py
@@ -0,0 +1,37 @@
+"""
+@author Corentin Goetghebeur (github.com/CorentinGoet)
+"""
+from enum import Enum
+
+
+class Declaration:
+    """
+    Class representation of a Declaration.
+    """
+
+    def __init__(self, type, identifier, integer=None):
+        self.type = type
+        self.identifier = identifier
+        self.integer = integer
+
+    def accept(self, visitor):
+        """
+        Method to accept vist by the visitor
+        :param visitor: visitor
+        :return: None
+        """
+        visitor.visitDeclaration(self)
+
+
+
+class Types(Enum):
+    """
+    Enum of types.
+    """
+    INT = 1
+    BOOL = 2
+    FLOAT = 3
+    CHAR = 4
+
+if __name__ == '__main__':
+    pass
---
+++ declarations.py
@@ -0,0 +1,24 @@
+"""
+@author Corentin Goetghebeur (github.com/CorentinGoet)
+"""
+
+import ast
+
+class Declarations:
+    """
+    Class representation of the Declarations Node.
+    """
+
+    def __init__(self):
+        self.declaration = []
+
+    def accept(self, visitor):
+        visitor.visitDeclarations(self)
+
+    def addDeclaration(self, declaration):
+        self.declaration.append(declaration)
+
+
+
+if __name__ == '__main__':
+    pass
