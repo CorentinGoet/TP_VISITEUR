09f3f9dcba9202c1828f5ae4e0f8a476dc222f9c
CorentinGoet <corentin.goetghebeur@ensta-bretagne.org>
2022-03-02 17:15:19 +0100

    added new lexer

--- visiteur.py
+++ visiteur.py
@@ -2,11 +2,14 @@
 @author Corentin Goetghebeur (github.com/CorentinGoet)
 """
 import ast
+from mini_c_lexer import Lexer
 
 class Visiteur:
-
+    pass
 
 if __name__ == '__main__':
-
-
-    pass
+    file = open("test_minic.txt")
+    src = file.readlines()
+    lex = Lexer()
+    lex.lex(src)
+    print(lex.lexems)
